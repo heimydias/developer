621ab9c9d0b6e9891651b36db9761d407a80b352
Heimy Dias <heimysantana@hotmail.com>
2023-05-11 21:17:02 -0300

    meu primeiro projeto com React

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Home from './paginas/home/Home'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
+);
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './Home.css'
+
+function Home(){
+    return(
+    <>
+        <h1 className="titulo">Hello, I'm developer!</h1>
+        <img className='image' src='src/assets/image/Logo.png' alt='foto'/>
+    </>
+    )
+}
+
+export default Home;
